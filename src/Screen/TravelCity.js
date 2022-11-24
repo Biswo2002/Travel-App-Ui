8fe8c001342dfc655b33ef557e5f2c54fc343620
Biswopaban Nayak <biswopabannayak@gmail.com>
2022-11-24 12:21:43 +0530

    Travel Ui

---
+++ src/Screen/TravelCity.js
@@ -0,0 +1,16 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AntDesign from ''
+const TravelCity = () => {
+  return (
+    <SafeAreaView>
+      <View style={styles.FirstContainer}>
+        {/* <AntDesign/> */}
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default TravelCity
+
+const styles = StyleSheet.create({})
